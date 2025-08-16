e8f826f3420408e8ff3ae8467a7da74b9738d7cc
AlexDz <alexdzdevelop@gmail.com>
2025-08-16 19:14:00 +0300

    Add items to db

---
+++ js/db.js
@@ -0,0 +1,46 @@
+const productsData = {
+  topSellers: [
+    {
+		id: 1,
+		title: "Boat Rockerz 333",
+		price: 20,
+		discount: "60%",
+		img: "../img/main/topSellers/first.svg"
+	},
+    {
+		id: 2,
+		title: "Boat kerz 234",
+		price: 40,
+		discount: null,
+		img: "../img/main/topSellers/second.svg"
+	},
+    {
+		id: 3,
+		title: "Boat Rockerz 323",
+		price: 30,
+		discount: "40%",
+		img: "../img/main/topSellers/third.svg"
+	},
+    {
+		id: 4,
+		title: "Boat Rockerz 600",
+		price: 33,
+		discount: "22%",
+		img: "../img/main/topSellers/fourth.png"
+	},
+    {
+		id: 5,
+		title: "Boat Rockerz 550",
+		price: 70,
+		discount: "10%",
+		img: "../img/main/topSellers/fiveth.png"
+	},
+
+
+  ],
+  trendingEarphones: [
+    { id: 10, title: "Boat Rockerz 323", price: 30, discount: "40%", img: "img10.png" },
+    ...
+  ]
+};
+export default productsData;
